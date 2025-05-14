fdab613c41e0ed62c87adf547e5d02eb5c9b1bcc
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-05-14 19:08:15 +0530

    add vercel.json file and fix deployment error

--- EcoRecycle-main/vite.config.js
+++ EcoRecycle-main/vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0", // Bind to all network interfaces
-    port: 5173, // You can change the port if needed
-  },
 });
